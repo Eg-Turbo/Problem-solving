ded3d889eea61dfcc13b4ed3d490870856093c73
Turbo <xpboody@gmail.com>
2022-07-17 14:57:08 +0200

    compareTriplets problem

---
+++ Hacker Rank/compareTriplets.js
@@ -0,0 +1,1 @@
+ function compareTriplets(a, b) {     // Write your code here     let result = [0,0]     for(let i = 0 ; i<a.length ; i++) {         if(a[i] > b[i]) result[0]+=1         else if (a[i] < b[i]) result[1] +=1         else continue     }     return result  }
